c7a5d9289b7669037918936792ec7431ba30cd0a
astutichoudhary <astutichoudhary31@gmail.com>
2026-06-26 00:26:46 +0530

    Final code. Number guessing game.

--- code.py
+++ code.py
@@ -1,15 +1,17 @@
 import random
 
-
 def game():
+    print("----Welcome to The Ultimate Number Guesser Game----")
+
     low = int(input("Enter lower limit of your range: "))
     high = int(input("Enter upper limit of your range: "))
 
     print("You now have 7 chances to guess your number.")
+
     num = random.randint(low,high)
     i = 1
     for i in range(1,8):
-        guess_num = int(input("Enter your guess number: "))
+        guess_num = int(input(f"{i}. Enter your guess number: "))
         if guess_num==num:
             print(f"You got it.{num} is the correct number. Congratulations!\n")
             end()
@@ -20,29 +22,19 @@ def game():
             print("Too low.")
         else:
             print(f"Enter a valid integer between {low} and {high}")
-        i += 1
-    if i==7 and guess_num!=num:
+
+    for i in range(7,8):
         print(f"Sorry! You failed at guessing the number. The number was {num}")
+        end()
 
-print("----Welcome to The Ultimate Number Guesser Game----")
-
-game()
 
 def end():
-    end = input("Press Enter for replay and q for exiting the game.")
-while True:
-    if end=="":
-        game()
-    elif end=="q":
-        break
-    else:
-        print("Invalid.")
-        end()
-     
-     
+    end = input("Press Enter for replay.\n")
+    while True:
+        if end=="":
+            game()
+        else:
+            break
 
-
-
-
-
-
+game()
+end()
